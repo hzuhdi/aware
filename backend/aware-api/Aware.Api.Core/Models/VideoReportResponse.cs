132967b4b0073285240120a1f4206c57b8eb77ff
g.malik <g.malik@brainbay.nl>
2021-11-26 21:50:38 +0100

    Add a starting point for bridging python and .NET

---
+++ backend/aware-api/Aware.Api.Core/Models/VideoReportResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aware.Api.Core.Models
+{
+    public class VideoReportResponse : VideoReportRequest
+    {
+        public DateTime ProcessedDate { get; private set; } = DateTime.UtcNow;
+    }
+}
---
+++ backend/aware-api/Aware.Api.MachineLearningClient/Clients/VideoDeepwareDetectionClient.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+using Aware.Api.Core.Interfaces;
+using Aware.Api.Core.Models;
+
+namespace Aware.Api.MachineLearningClient.Clients
+{
+    public class VideoDeepwareDetectionClient : PythonClientBase, IPythonClient<VideoReportRequest, VideoReportResponse>
+    {
+        public VideoDeepwareDetectionClient() : base() { }
+
+        public async Task<VideoReportResponse?> ExecuteAsync(VideoReportRequest requestModel, CancellationToken cancellationToken = default)
+        {
+            var script = GetScript();
+            await InitializeEngineWithScript(script, cancellationToken);
+
+            var analyzeVideo = _scope?.GetVariable<Func<string, bool, string>>("analyzeVideo");
+            if (analyzeVideo == null)
+            {
+                throw new ArgumentNullException(nameof(analyzeVideo));
+            }
+            string? result = null;
+
+            await Task.Factory.StartNew(() => result = analyzeVideo(requestModel.Url, requestModel.HasDeepfake));
+
+            if (string.IsNullOrEmpty(result)) return null;
+
+            return JsonSerializer.Deserialize<VideoReportResponse>(result);
+        }
+
+        private static string GetScript()
+        {
+            return $@"
+import json
+import time
+
+class VideoAnalysisReport:
+    def __init__(self, url, hasDeepfake):
+      self.url = url
+      self.hasDeepfake = hasDeepfake
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, 
+            sort_keys=True, indent=4)
+                
+def analyzeVideo(url, hasDeepfake):
+    time.sleep(3)
+    return VideoAnalysisReport(url, hasDeepfake).toJSON()
+            ";
+        }
+    }
+}
