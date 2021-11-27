7fff5e619376b6903d0c3700d6fa489a8d1ec83a
Hafizh Ahmad Zuhdi <hafizhahmadzuhdi@gmail.com>
2021-11-27 17:16:53 +0100

    Remove unused code

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,18 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+import { createStore } from 'redux';
 import reducer from './reducer';
 
 // ==============================|| REDUX - MAIN STORE ||============================== //
 
-const loggerMiddleware = createLogger();
-
 const store = createStore(
-    reducer,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    )
+    reducer
 );
 const persister = 'Free';
 
